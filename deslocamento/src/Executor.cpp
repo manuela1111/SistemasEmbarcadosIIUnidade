4e79e9d89a5dc10904a22bce991f78eba8189d1a
manuela1111 <mn2192@yahoo.com.br>
2015-11-29 09:13:06 -0300

    Inserida struct com os dados obtidos;
    Adicionado laço de repetição para imprimir os dados enviados pelos
    sensores. (Código  parando após algumas execuções, realizar correção
    solicitada pelo professor no e-mail)

--- deslocamento/src/Executor.cpp
+++ deslocamento/src/Executor.cpp
@@ -9,48 +9,38 @@
 
 using namespace std;
 
+struct InfoRF{
+  short id; //o int do arduino é short
+  short batimentos;
+  short temperatura;
+  boolean movimento;
+};
+
 int main (int argc, char** argv){
 
-	int temperatura = 36;
-	int pressaoSistolica = 8;
-	int pressaoDiastolica = 12;
-	int batimentos = 100;
+	//criar uma instancia da classe de comunicação
+	//#ifdef __linux__
+	//	Comunicacao com = Comunicacao("/dev/ttyACM0");
+	//#endif
+	//#ifdef _WIN32 || _WIN64
+	//	Comunicacao com = Comunicacao("COM6");
+	//#endif
 
-	//o int do c++ é o long do arduino
-	int info = 0;
+		InfoRF info = {0};
 
-	//o sizeof do int é 4 bytes
-	cout <<"tam = "<<sizeof(info)<<endl;
+		Comunicacao com = Comunicacao("COM6");
 
-	//esse | não é de condicional é de operação binária entre o info e cada um
-	//int info             00000000 00000000 00000000 00000000
-	//temp é 36 em binário 00100100 preenche com os zeros
-	//o próximo deslocamento é 16
-	//pressao Sistolica             00001000
+		com.iniciar();
 
-	info = info | (temperatura << 24);
-	info = info | (pressaoSistolica << 16);
-	info = info | (pressaoDiastolica << 8);
-	info = info | batimentos;
+		 //coloca zero para todas as posições
+		while(true){
+			if(com.ler((char*)&info, sizeof(InfoRF)) == 0){
+				cout <<"id = "<< info.id << endl;
+				cout <<"temp = "<< info.temperatura << endl;
+				cout <<"batm = "<< info.batimentos << endl;
+				cout <<"movimento = "<< info.movimento << endl;
+			}
 
-	cout <<"info = "<<info<<endl;
-
-	//informação enviada pelo RF Transmissor
-
-	//vamos recuperar as informações usando o & lógico
-	//4278190000 equivale a 11111111 00000000 00000000 00000000
-	//16711680 equivale
-	int temperaturaRec = (info & 4278190000)>>24;
-	//para pegar a pressão sistolica desloca o 1111111 para a direita
-	int pressaoSistolicaRec = (info & 16711680)>>16;
-	int pressaoDiastolicaRec = (info & 65280)>>8;
-	int batimentosRec = (info & 255);
-
-	//teste
-
-	//informação decodifcada pelo receptor
-	cout <<"Temperatura = "<<temperaturaRec<<endl;
-	cout <<" Pressão Sistólica = "<<pressaoSistolicaRec<<endl;
-	cout <<"Pressão Diastólica = "<<pressaoDiastolicaRec<<endl;
-	cout <<"Batimentos = "<<batimentosRec<<endl;
+			Sleep(50);
+		}
 }
